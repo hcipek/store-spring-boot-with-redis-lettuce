7aae9c6031c0e24014a29a508b6103fc93041cf6
hipek-ebebek <h.ipek@ebebek.com>
2021-02-21 02:00:59 +0300

    19.02.21 Developments at Work

--- src/main/java/com/ebebek/reactiveredis/config/StoreConfiguration.java
+++ src/main/java/com/ebebek/reactiveredis/config/StoreConfiguration.java
@@ -3,7 +3,9 @@ package com.ebebek.reactiveredis.config;
 import com.ebebek.reactiveredis.model.Store;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.connection.*;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.ReactiveRedisOperations;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -12,16 +14,36 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class StoreConfiguration {
+
     @Bean
-    ReactiveRedisOperations<String, Store> redisOperations(ReactiveRedisConnectionFactory factory) {
-        Jackson2JsonRedisSerializer<Store> serializer = new Jackson2JsonRedisSerializer<>(Store.class);
+    public LettuceConnectionFactory redisConnectionFactory() {
 
-        RedisSerializationContext.RedisSerializationContextBuilder<String, Store> builder =
-                RedisSerializationContext.newSerializationContext(new StringRedisSerializer());
-
-        RedisSerializationContext<String, Store> context = builder.value(serializer).build();
-
-        return new ReactiveRedisTemplate<>(factory, context);
+        return new LettuceConnectionFactory(new RedisStandaloneConfiguration("server", 6379));
     }
 
+//    WriteToMasterReadFromReplicaConfiguration
+    /*
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory2() {
+
+        LettuceClientConfiguration clientConfig = LettuceClientConfiguration.builder()
+                .readFrom(SLAVE_PREFERRED)
+                .build();
+
+        RedisStandaloneConfiguration serverConfig = new RedisStandaloneConfiguration("server", 6379);
+
+        return new LettuceConnectionFactory(serverConfig, clientConfig);
+    }
+    */
+//    RedisConfiguration.SentinelConfiguration
+    /*
+    @Bean
+    public RedisConnectionFactory lettuceConnectionFactory3() {
+        RedisSentinelConfiguration sentinelConfig = new RedisSentinelConfiguration()
+                .master("mymaster")
+                .sentinel("127.0.0.1", 26379)
+                .sentinel("127.0.0.1", 26380);
+        return new LettuceConnectionFactory(sentinelConfig);
+    }
+    */
 }
